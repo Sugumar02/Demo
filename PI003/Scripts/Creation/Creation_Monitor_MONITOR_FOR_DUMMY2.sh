614e3c153701fea0434a158005710552dc90b680
Sugumar02 <suraja@primark.co.uk>
2018-08-02 12:03:58 +0530

    Update and rename Creation_Monitor_MONITOR_FOR_DUMMY2.sh to Creation_Monitor_MONITOR_FOR_DUMMY2_Test.sh

--- PI003/Scripts/Creation/Creation_Monitor_MONITOR_FOR_DUMMY2.sh
+++
@@ -1,1 +0,0 @@
-fteCreateMonitor -p QDT1MFT1 -ma FTE_LOCAL_FTP -mn MONITOR_FOR_DUMMY2 -md /home/smohan/dummy/test/ -pi 1 -tr "noSizeChange=2,*.TXT" -mt /home/mqm/MQFTE/MonitorProfile/PI003/Task_Monitor_MONITOR_FOR_DUMMY2.xml
