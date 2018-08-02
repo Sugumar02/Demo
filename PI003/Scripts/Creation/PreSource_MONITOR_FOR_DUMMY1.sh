3ff7390b9af9769d9df0378d6577743e05d106a5
Sugumar02 <suraja@primark.co.uk>
2018-08-02 12:04:29 +0530

    Rename PreSource_MONITOR_FOR_DUMMY1.sh to PreSource_MONITOR_FOR_DUMMY1_Test.sh

--- PI003/Scripts/Creation/PreSource_MONITOR_FOR_DUMMY1.sh
+++
@@ -1,1 +0,0 @@
-fteCreateTransfer -p QDT1MFT1 -sa FTE_LOCAL_FTP -sm QDT1MFT1 -da FTE_LOCAL_FTP -dm QDT1MFT1 -jn MFT_Monitor_Presource -sd delete -de overwrite -dd /home/smohan/dummy/test /home/smohan/dummy/test/*.TXT
