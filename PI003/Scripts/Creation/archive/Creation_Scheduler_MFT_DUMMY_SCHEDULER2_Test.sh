48c8b9ffc467290f2b23651a4977731e4c4be5fd
Sugumar02 <suraja@primark.co.uk>
2018-09-12 12:20:20 +0530

    Deleted
    Deleted

--- PI003/Scripts/Creation/archive/Creation_Scheduler_MFT_DUMMY_SCHEDULER2_Test.sh
+++
@@ -1,1 +0,0 @@
-fteCreateTransfer -p QDT1MFT1 -sa FTE_LOCAL_FTP -sm QDT1MFT1 -da FTE_CLIENT_FTP -dm QDT1MFT1 -sd delete -de error -jn MFT_DUMMY_SCHEDULER2 -presrc executable:/home/mqm/Script/PreSource_MFT_DUMMY_SCHEDULER1_Test.sh -ss 00:00 -tb source -oi minutes -of 5 -dd /home/smohan/dummy/test/ /home/smohan/dummy/test/test*.*
